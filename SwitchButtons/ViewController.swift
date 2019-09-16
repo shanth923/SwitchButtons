c8a7739d6c665a4e38d99aec4c5d414a4bcc8d45
R Shantha Kumar <shanth923@achiever-104.local>
2019-09-16 16:43:35 +0530

    Initial Commit

---
+++ SwitchButtons/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SwitchButtons
+//
+//  Created by R Shantha Kumar on 9/16/19.
+//  Copyright © 2019 R Shantha Kumar. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
